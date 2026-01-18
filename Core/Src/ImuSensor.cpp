8f7412808aaa509c855630e1b9011c37fbeea93e
Developper2310 <yufurkan.2310@gmail.com>
2026-01-18 20:59:13 +0300

    Implement ImuSensor class structure for MPU6050 (Accel only)

---
+++ Core/Src/ImuSensor.cpp
@@ -0,0 +1,59 @@
+/*
+ * ImuSensor.cpp
+ *
+ *  Created on: Jan 18, 2026
+ *      Author: yufur
+ */
+
+#include "ImuSensor.h"
+#include <math.h>
+
+#define MPU6050_ADDR         0xD0 // (0x68 << 1)
+#define PWR_MGMT_1_REG       0x6B // wake point
+#define ACCEL_XOUT_H_REG     0x3B // sector that we read values
+
+
+
+ImuSensor::ImuSensor(I2C_HandleTypeDef *hi2c) {
+	// TODO Auto-generated constructor stub
+	_i2c=hi2c;
+}
+
+bool ImuSensor::init() {
+	uint8_t uyandirici = 0;
+
+
+	mpu_status=HAL_I2C_Mem_Write(_i2c,MPU6050_ADDR,PWR_MGMT_1_REG,1, &uyandirici,1,100);
+
+	if(mpu_status == HAL_OK)return 1;
+	return 0;
+}
+
+
+
+void ImuSensor::readAccel(){
+	uint8_t buffer[6];
+
+	HAL_StatusTypeDef status = HAL_I2C_Mem_Read(_i2c, MPU6050_ADDR, ACCEL_XOUT_H_REG, 1, buffer, 6, 100);
+
+	if(status==HAL_OK){
+		mpu_data.AccX=((float)((int16_t)(buffer[0] << 8 | buffer[1]))/16384.0);
+		mpu_data.AccY=((float)((int16_t)(buffer[2] << 8 | buffer[3]))/16384.0);
+		mpu_data.AccZ=((float)((int16_t)(buffer[4] << 8 | buffer[5]))/16384.0);
+
+		if(mpu_data.AccX==0 && mpu_data.AccY==0 && mpu_data.AccZ==0)return ; // i2c line reboot
+
+		mpu_data.totalforce = sqrt((mpu_data.AccX * mpu_data.AccX) + (mpu_data.AccY * mpu_data.AccY) + (mpu_data.AccZ * mpu_data.AccZ));
+
+	}
+
+}
+
+MPU_DATA ImuSensor::getData() {
+    return mpu_data;
+}
+
+void ImuSensor::readGyro(){
+
+}
+
